cce3d356b36f6538fb768dba76bb6299f48acde1
he0o0nje <dudwogjswp@naver.com>
2023-08-09 17:29:00 +0900

    8-9(1)

--- js/imgslide.js
+++ js/imgslide.js
@@ -1,73 +1,73 @@
 window.onload = function () {
-    const imgslide = document.querySelector('.imgslide');
-    const slider = imgslide.querySelector('.slider');
-    const slideli = slider.querySelectorAll('li');
-    const controller = imgslide.querySelector('.controller');
+  const imgslide = document.querySelector(".imgslide");
+  const slider = imgslide.querySelector(".slider");
+  const slideli = slider.querySelectorAll("li");
+  const controller = imgslide.querySelector(".controller");
 
-    const copy1 = slideli[0].cloneNode(true);
-    const copylast = slideli[slideli.length - 1].cloneNode(true);
-    slider.insertBefore(copylast, slideli[0]);
-    slider.appendChild(copy1);
+  const copy1 = slideli[0].cloneNode(true);
+  const copylast = slideli[slideli.length - 1].cloneNode(true);
+  slider.insertBefore(copylast, slideli[0]);
+  slider.appendChild(copy1);
 
-    let currentIdx = 0;
-    let translate = 0;
-    const speedTime = 500;
+  let currentIdx = 0;
+  let translate = 0;
+  const speedTime = 500;
 
-    const slidercopyli = slider.querySelectorAll('li');
-    const liWidth = slideli[0].clientWidth;
-    const sliderwidth = liWidth * slidercopyli.length;
-    slider.style.width = `${sliderwidth}px`;
-    currentIdx = 1;
-    translate = -liWidth;
+  const slidercopyli = slider.querySelectorAll("li");
+  const liWidth = slideli[0].clientWidth;
+  const sliderwidth = liWidth * slidercopyli.length;
+  slider.style.width = `${sliderwidth}px`;
+  currentIdx = 1;
+  translate = -liWidth;
+  slider.style.transform = `translateX(${translate}px)`;
+
+  controller.addEventListener("click", moveSlide);
+
+  function move(D) {
+    currentIdx += -1 * D;
+    translate += liWidth * D;
     slider.style.transform = `translateX(${translate}px)`;
+    slider.style.transition = `all ${speedTime}ms ease`;
+  }
 
-    controller.addEventListener('click', moveSlide);
+  function moveSlide(event) {
+    event.preventDefault();
+    if (event.target.className === "next") {
+      move(-1);
+      if (currentIdx === slidercopyli.length - 1)
+        setTimeout(() => {
+          slider.style.transition = "none";
+          currentIdx = 1;
+          translate = -liWidth;
+          slider.style.transform = `translateX(${translate}px)`;
+        }, speedTime);
+    } else {
+      move(1);
+      if (currentIdx === 0) {
+        setTimeout(() => {
+          slider.style.transition = "none";
+          currentIdx = slidercopyli.length - 2;
+          translate = -(liWidth * currentIdx);
+          slider.style.transform = `translateX(${translate}px)`;
+        }, speedTime);
+      }
+    }
+  }
 
-    function move(D) {
-        currentIdx += (-1 * D);
-        translate += liWidth * D;
+  function autoslide() {
+    move(-1);
+    if (currentIdx === slidercopyli.length - 1)
+      setTimeout(() => {
+        slider.style.transition = "none";
+        currentIdx = 1;
+        translate = -liWidth;
         slider.style.transform = `translateX(${translate}px)`;
-        slider.style.transition = `all ${speedTime}ms ease`
-      }
-  
-      function moveSlide(event) {
-        event.preventDefault();
-        if (event.target.className === 'next') {
-          move(-1);
-          if (currentIdx === slidercopyli.length -1)
-            setTimeout(() => {
-              slider.style.transition = 'none';
-              currentIdx = 1;
-              translate = -liWidth;
-              slider.style.transform = `translateX(${translate}px)`;
-            }, speedTime);
-        } else {
-            move(1);
-            if (currentIdx === 0) {
-              setTimeout(() => {
-                slider.style.transition = 'none';
-                currentIdx = slidercopyli.length -2;
-                translate = -(liWidth * currentIdx);
-                slider.style.transform = `translateX(${translate}px)`;
-              }, speedTime);
-            }
-          }
-      }
+      }, speedTime);
+  }
 
-      function autoslide() {
-        move(-1);
-        if (currentIdx === slidercopyli.length -1)
-          setTimeout(() => {
-            slider.style.transition = 'none';
-            currentIdx = 1;
-            translate = -liWidth;
-            slider.style.transform = `translateX(${translate}px)`;
-          }, speedTime);
-      }
+  function sliding() {
+    setInterval(autoslide, 2000);
+  }
 
-      function sliding() {
-        setInterval(autoslide, 2000);
-      }
-
-      sliding();
+  sliding();
 };
