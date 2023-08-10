65e637abc958c579a835772a854ba9ff51a33632
he0o0nje <dudwogjswp@naver.com>
2023-08-10 11:32:58 +0900

    8-10(1)

--- js/main.js
+++ js/main.js
@@ -2,4 +2,16 @@ $(document).ready(function () {
   $(".hamburger_menu").click(function () {
     $(".hamburger_nav").slideToggle();
   });
+
+  $(window).resize(function () {
+    if (window.innerWidth >= 1025) {
+      $(".hamburger_nav").hide();
+    }
+  });
+
+  // $(window).resize(function () {
+  //   if (window.innerWidth <= 1024) {
+  //     $(".hamburger_nav").hide();
+  //   }
+  // });
 });
